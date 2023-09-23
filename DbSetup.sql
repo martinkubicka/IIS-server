a73960bd5685085fe80509e9954742981963ffa2
martinkubicka <martinkubicka22@gmail.com>
2023-09-23 11:13:23 +0200

    add group members into one table + create trigger for checking if member already in group

--- DbSetup.sql
+++ DbSetup.sql
@@ -1,17 +1,18 @@
 DROP TABLE Rating;
 DROP TABLE Post;
 DROP TABLE Thread;
-DROP TABLE Moderator;
 DROP TABLE Member;
 DROP TABLE Groups;
 DROP TABLE Users;
+DROP PROCEDURE DeleteUser;
+DROP TRIGGER CheckDuplicateMembers;
 
 -- TABLES
 
 CREATE TABLE Users (
     Email VARCHAR(255) NOT NULL PRIMARY KEY,
     Password VARCHAR(255) NOT NULL,
-    Handle VARCHAR(255) NOT NULL UNIQUE,
+    Handle VARCHAR(255) NOT NULL,
     Name VARCHAR(255) NOT NULL,
     Role INT NOT NULL,
     VisibilityRegistered BOOLEAN DEFAULT TRUE,
@@ -20,7 +21,7 @@ CREATE TABLE Users (
     Icon VARCHAR(255)
 );
 
-CREATE TABLE Groups (
+CREATE TABLE `Groups` (
     Handle VARCHAR(255) NOT NULL PRIMARY KEY,
     Description VARCHAR(255),
     Name VARCHAR(255) NOT NULL,
@@ -35,16 +36,9 @@ CREATE TABLE Member (
     Id VARCHAR(255) NOT NULL PRIMARY KEY,
     Handle VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL,
+    GroupRole INT NOT NULL,
     FOREIGN KEY (Email) REFERENCES Users(Email),
-    FOREIGN KEY (Handle) REFERENCES Groups(Handle)
-);
-
-CREATE TABLE Moderator (
-    Id VARCHAR(255) NOT NULL PRIMARY KEY,
-    Handle VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    FOREIGN KEY (Email) REFERENCES Users(Email),
-    FOREIGN KEY (Handle) REFERENCES Groups(Handle)
+    FOREIGN KEY (Handle) REFERENCES `Groups`(Handle)
 );
 
 CREATE TABLE Thread (
@@ -54,7 +48,7 @@ CREATE TABLE Thread (
     Name VARCHAR(255) NOT NULL,
     Date DATETIME NOT NULL,
     FOREIGN KEY (Email) REFERENCES Users(Email),
-    FOREIGN KEY (Handle) REFERENCES Groups(Handle)
+    FOREIGN KEY (Handle) REFERENCES `Groups`(Handle)
 );
 
 CREATE TABLE Post (
@@ -84,18 +78,35 @@ CREATE PROCEDURE DeleteUser(IN userEmail VARCHAR(255))
 BEGIN
     DECLARE isAdmin INT DEFAULT 0;
     
-    SELECT COUNT(*) INTO isAdmin FROM Groups WHERE Admin = userEmail;
+    SELECT COUNT(*) INTO isAdmin FROM `Groups` WHERE Admin = userEmail;
 
     IF isAdmin > 0 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'User is an admin and cannot be deleted';
     ELSE
-        DELETE FROM Moderator WHERE Email = userEmail;
-
         DELETE FROM Member WHERE Email = userEmail;
 
         DELETE FROM Users WHERE Email = userEmail;
     END IF;
 END //
+
+DELIMITER ;
+
+-- TRIGGERS
+
+DELIMITER //
+
+CREATE TRIGGER CheckDuplicateMembers
+BEFORE INSERT ON Member
+FOR EACH ROW
+BEGIN
+    DECLARE handle_count INT;
+    SET handle_count = (SELECT COUNT(*) FROM Member WHERE Handle = NEW.Handle AND Email = NEW.Email);
+    
+    IF handle_count > 0 THEN
+        SIGNAL SQLSTATE '45001'
+        SET MESSAGE_TEXT = 'User already exists in this group.';
+    END IF;
+END; //
 
 DELIMITER ;
