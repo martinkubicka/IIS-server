c4b1fde5644dc9e48097481a92ceb111d8ffdf45
xmacek27 <matej.macek@email.cz>
2023-09-23 22:00:34 +0200

    Add Thread Endpoints

---
+++ IIS_SERVER/IIS_SERVER/Thread/Models/ThreadModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IIS_SERVER.Thread.Models
+{
+    public class ThreadModel
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string Handle { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
