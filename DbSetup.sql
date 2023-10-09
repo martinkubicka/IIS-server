b75f6cb82db719f62e1034ffc147253cfa8a5300
martinkubicka <martinkubicka22@gmail.com>
2023-10-09 23:37:19 +0200

    add more users and members

--- DbSetup.sql
+++ DbSetup.sql
@@ -180,13 +180,15 @@ END; //
 DELIMITER ;
 
 -- INSERTS
--- Insert sample user data
+-- Insert additional sample user data to make at least six users
 INSERT INTO Users (Id, Email, Password, Handle, Name, Role, VisibilityRegistered, VisibilityGuest, VisibilityGroup, Icon)
 VALUES
-    -- all passwords set to "password"
     ('85e619ff-1593-4fae-a4e1-8b5268c4c9a1', 'user1@example.com', '$2a$10$YWX0i1FngYLmVmpoBMzZZujAP0dnRDmGRjtcfNa11ryxEezzQepBa', 'user1_handle', 'User 1', 1, TRUE, TRUE, TRUE, 'user1-icon'),
     ('b9d3a8e4-3a62-4b21-a5c8-5ebc0b3bf929', 'user2@example.com', '$2a$10$YWX0i1FngYLmVmpoBMzZZujAP0dnRDmGRjtcfNa11ryxEezzQepBa', 'user2_handle', 'User 2', 1, TRUE, TRUE, TRUE, 'user2-icon'),
-    ('e7ac07c1-7dcd-4b36-b2a3-1f676f10a0ab', 'user3@example.com', '$2a$10$YWX0i1FngYLmVmpoBMzZZujAP0dnRDmGRjtcfNa11ryxEezzQepBa', 'user3_handle', 'User 3', 1, TRUE, TRUE, TRUE, 'user3-icon');
+    ('e7ac07c1-7dcd-4b36-b2a3-1f676f10a0ab', 'user3@example.com', '$2a$10$YWX0i1FngYLmVmpoBMzZZujAP0dnRDmGRjtcfNa11ryxEezzQepBa', 'user3_handle', 'User 3', 1, TRUE, TRUE, TRUE, 'user3-icon'),
+    ('f5e619ff-1593-4fae-a4e1-8b5268c4c9a1', 'user4@example.com', '$2a$10$YWX0i1FngYLmVmpoBMzZZujAP0dnRDmGRjtcfNa11ryxEezzQepBa', 'user4_handle', 'User 4', 1, TRUE, TRUE, TRUE, 'user4-icon'),
+    ('b9d3a8e4-3a62-4b21-a5c8-5ebc0b3bf939', 'user5@example.com', '$2a$10$YWX0i1FngYLmVmpoBMzZZujAP0dnRDmGRjtcfNa11ryxEezzQepBa', 'user5_handle', 'User 5', 1, TRUE, TRUE, TRUE, 'user5-icon'),
+    ('e7ac07c1-7dcd-4b36-b2a3-1f676f11a0ab', 'user6@example.com', '$2a$10$YWX0i1FngYLmVmpoBMzZZujAP0dnRDmGRjtcfNa11ryxEezzQepBa', 'user6_handle', 'User 6', 1, TRUE, TRUE, TRUE, 'user6-icon');
 
 -- Insert sample group data
 INSERT INTO `Groups` (Id, Handle, Description, Name, VisibilityMember, VisibilityGuest, Icon)
@@ -194,13 +196,19 @@ VALUES
     ('50ca0201-3a6b-4d5b-9e6c-6899a0f0aa7f', 'group1', 'Description for Group 1', 'Group 1', TRUE, TRUE, 'group1-icon'),
     ('da28ee79-bc45-4380-a0dd-7c8f0bfc8b6f', 'group2', 'Description for Group 2', 'Group 2', TRUE, TRUE, 'group2-icon');
 
--- Insert sample member data
+-- Insert additional sample member data to make at least six members
 INSERT INTO Member (Id, Handle, Email, GroupRole)
 VALUES
-    ('a9cfbc31-1c33-446d-bcae-8c055108b704', 'group1', 'user1@example.com', 1),
-    ('f9d8f37c-69e5-4e64-a3f0-b006b1a88cb4', 'group1', 'user2@example.com', 2),
-    ('e2a2355c-9da3-45a0-85e7-8c4e24a9d8f6', 'group2', 'user2@example.com', 1),
-    ('8196b4db-5f06-4f3e-9e63-2a752d3b1ef5', 'group2', 'user3@example.com', 2);
+    ('ab9cfbc31-1c33-446d-bcae-8c045108b704', 'group1', 'user4@example.com', 1),
+    ('cf9d8f37c-69e5-4e64-a3f0-b006b1a88cb4', 'group1', 'user5@example.com', 2),
+    ('8f5de2a3-7a0b-4f29-9842-5c2727e27483', 'group1', 'user6@example.com', 1),
+    ('ab9cfbc31-1c33-446d-bcae-8c055108b704', 'group2', 'user1@example.com', 1),
+    ('cf9d8f37c-69e5-4e64-a3f0-b007b1a88cb4', 'group2', 'user2@example.com', 2),
+    ('8f5de2a3-7a0b-4f29-9842-5c2727e27473', 'group2', 'user3@example.com', 1),
+    ('de2a2355c-9da3-45a0-85e7-8c4e24a9d8f6', 'group2', 'user4@example.com', 1),
+    ('c8196b4db-5f06-4f3e-9e63-2a752d3b1ef5', 'group2', 'user5@example.com', 2),
+    ('0e12a7cf-25ea-468c-af4e-9f0b4f18f4de', 'group2', 'user6@example.com', 2);
+
 
 -- Insert sample thread data
 INSERT INTO Thread (Id, Description, Handle, Email, Name, Date)
