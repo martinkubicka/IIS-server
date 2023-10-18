1161db976c2bfe01e82bc7b001710c7b533c5668
martinkubicka <martinkubicka22@gmail.com>
2023-10-18 20:21:29 +0200

    add conf to service

--- IIS_SERVER/IIS_SERVER/Services/MySQLService.cs
+++ IIS_SERVER/IIS_SERVER/Services/MySQLService.cs
@@ -6,12 +6,14 @@ public partial class MySQLService : IDisposable
 {
     private readonly string ConnectionString;
     private readonly MySqlConnection Connection;
+    private readonly IConfiguration Configuration;
 
     public MySQLService(IConfiguration configuration)
     {
         ConnectionString = configuration["DB:ConnectionString"];
         Connection = new MySqlConnection(ConnectionString);
-
+        Configuration = configuration;
+        
         Connection.Open();
     }
 
