1bfd6d6ace2586704db7ab1d3ab32aeffe4716c2
martinkubicka <martinkubicka22@gmail.com>
2023-11-25 13:44:06 +0100

    change forgot password path to prod

--- IIS_SERVER/IIS_SERVER/Login/Controllers/LoginController.cs
+++ IIS_SERVER/IIS_SERVER/Login/Controllers/LoginController.cs
@@ -108,7 +108,7 @@ public class LoginController : ControllerBase, ILoginContoller
 
                 msg.Subject = "Password reset";
                 msg.IsBodyHtml = true;
-                msg.Body = string.Format("<html><head></head><body><b> https://musical-haupia-37aecd.netlify.app/passwordReset?token=" + result.Item2 + "</b></body>");
+                msg.Body = string.Format("<html><head></head><body><b> https://prod--musical-haupia-37aecd.netlify.app/passwordReset?token=" + result.Item2 + "</b></body>");
                 client.Send(msg);
             }
             catch (Exception ex)
